f9327be6e933a28a43e776f3b9f47db869130766
Anderson Begossi <andersonbegossi@gmail.com>
2021-11-08 08:37:40 -0300

    feat: create Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+import { UserPhoto } from '../UserPhoto';
+
+import LogoSvg from '../../assets/logo.svg';
+
+import { styles } from './styles';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <LogoSvg />
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+
+        <UserPhoto imageUri="https://github.com/ambegossi.png" />
+      </View>
+    </View>
+  );
+}
