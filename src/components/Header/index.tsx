e8907cbbb1feebef630511b155c4f4f548ec2749
Anderson Begossi <andersonbegossi@gmail.com>
2021-11-10 08:50:37 -0300

    feat: signOut

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,16 +10,18 @@ import LogoSvg from '../../assets/logo.svg';
 import { styles } from './styles';
 
 export function Header() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
   return (
     <View style={styles.container}>
       <LogoSvg />
 
       <View style={styles.logoutButton}>
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>Sair</Text>
-        </TouchableOpacity>
+        {user && (
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>Sair</Text>
+          </TouchableOpacity>
+        )}
 
         <UserPhoto imageUri={user?.avatar_url} />
       </View>
