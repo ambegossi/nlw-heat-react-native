d89a9bbc06ccd5ecd54d6921ba5057e2a2aa5243
Anderson Begossi <andersonbegossi@gmail.com>
2021-11-11 08:48:05 -0300

    feat: integrate SendMessage with api

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
-import { TextInput, View } from 'react-native';
+import { TextInput, View, Alert, Keyboard } from 'react-native';
 
 import { Button } from '../Button';
 
 import { COLORS } from '../../theme';
+import { api } from '../../services/api';
 
 import { styles } from './styles';
 
 export function SendMessageForm() {
   const [message, setMessage] = useState('');
   const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleMessageSubmit() {
+    const formattedMessage = message.trim();
+
+    if (formattedMessage.length > 0) {
+      setSendingMessage(true);
+
+      await api.post('/messages', { message: formattedMessage });
+
+      setMessage('');
+      Keyboard.dismiss();
+      setSendingMessage(false);
+      Alert.alert('Mensagem enviada com sucesso!');
+    } else {
+      Alert.alert('Escreva a mensagem para enviar.');
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -29,6 +47,8 @@ export function SendMessageForm() {
         title="ENVIAR MENSAGEM"
         backgroundColor={COLORS.PINK}
         color={COLORS.WHITE}
+        isLoading={sendingMessage}
+        onPress={handleMessageSubmit}
       />
     </View>
   );
